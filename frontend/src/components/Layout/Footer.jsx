c3dd4e1fe584ef02945e91dadf71bb15e97ae1fe
unknown <you@example.com>
2024-07-06 13:56:43 +0530

    first commit

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -8,18 +8,13 @@ const Footer = () => {
   const { isAuthorized } = useContext(Context);
   return (
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By CodeWithZeeshu.</div>
+      <div>&copy; All Rights Reserved By Nikhil.</div>
       <div>
-        <Link to={"https://www.facebook.com/profile.php?id=100030535123397"} target="_blank">
-          <FaFacebookF />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
-          <FaYoutube />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
+   
+        <Link to={"https://www.linkedin.com/in/nikhil-priyadarshi-437bb3266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"} target="_blank">
           <FaLinkedin />
         </Link>
-        <Link to={"https://www.instagram.com/z_4_zeeshuuu/"} target="_blank">
+        <Link to={"https://www.instagram.com/singh_nikhil06/"} target="_blank">
           <RiInstagramFill />
         </Link>
       </div>
